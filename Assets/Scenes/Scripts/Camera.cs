24a74311f0c288b53a37bbf8b6ac5377d98a7fae
Jan Kresic <jan.kresic@fer.hr>
2021-01-13 23:24:43 +0100

    Little bit of everything

---
+++ Assets/Scenes/Scripts/Camera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Transform>().Rotate(new Vector3(0, - rotateSpeed * Time.deltaTime, 0));
+        }
+       
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            GetComponent<Transform>().Rotate(new Vector3(-rotateSpeed * Time.deltaTime, 0, 0));
+        }
+       
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            GetComponent<Transform>().Rotate(new Vector3(rotateSpeed * Time.deltaTime, 0, 0));
+        }
+        
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Transform>().Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+        }
+    }
+}
